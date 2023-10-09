e1c7c1b5de1e62545da8fbfc12d3bbf6ebf2371a
Tl <linhuu8@gmail.com>
2023-10-09 11:18:49 +0700

    first commit

---
+++ src/ra/bussinessImp/Book.java
@@ -0,0 +1,204 @@
+package ra.bussinessImp;
+
+import jdk.internal.util.xml.impl.Input;
+import ra.bussiness.IBook;
+import ra.config.InputMethods;
+
+import static ra.run.BookManagement.books;
+
+
+public class Book implements IBook {
+    /* Các thuộc tính:
+▪ bookId – mã sách – int
+▪ bookName – tên sách – String
+▪ title – tiêu đề sách – String
+▪ numberOfPages – Số trang sách
+▪ importPrice – giá nhập sách – float
+▪ exportPrice – giá bán sách – float
+▪ interest – lợi nhuận – float
+▪ bookStatus – trạng thái – Boolean
+➢ Các constructor
+➢ Các phương thức:
+▪ Các phương thức getter/setter
+▪ Triển khai phương thức inputData():
+• Nhập thông tin sách từ bàn phím (trừ interest)
+• Thực hiện tính interest = exportPrice – importPrice
+▪ Triển khai phương thức displayData() cho phép hiển thị tất cả thông tin sách*/
+    private int bookId, numberOfPages;
+    private String bookName, title;
+    private float importPrice, exportPrice, interest;
+    private boolean bookStatus;
+
+    // get/set
+    public int getBookId() {
+        return bookId;
+    }
+
+    public void setBookId(int bookId) {
+        this.bookId = bookId;
+    }
+
+    public int getNumberOfPages() {
+        return numberOfPages;
+    }
+
+    public void setNumberOfPages(int numberOfPages) {
+        this.numberOfPages = numberOfPages;
+    }
+
+    public String getBookName() {
+        return bookName;
+    }
+
+    public void setBookName(String bookName) {
+        this.bookName = bookName;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public float getImportPrice() {
+        return importPrice;
+    }
+
+    public void setImportPrice(float importPrice) {
+        this.importPrice = importPrice;
+    }
+
+    public float getExportPrice() {
+        return exportPrice;
+    }
+
+    public void setExportPrice(float exportPrice) {
+        this.exportPrice = exportPrice;
+    }
+
+    public float getInterest() {
+        return interest;
+    }
+
+    public void setInterest(float interest) {
+        this.interest = interest;
+    }
+
+    public boolean isBookStatus() {
+        return bookStatus;
+    }
+
+    public void setBookStatus(boolean bookStatus) {
+        this.bookStatus = bookStatus;
+    }
+    // contructor
+
+    public Book(int bookId, int numberOfPages, String bookName, String title, float importPrice, float exportPrice, float interest, boolean bookStatus) {
+        this.bookId = bookId;
+        this.numberOfPages = numberOfPages;
+        this.bookName = bookName;
+        this.title = title;
+        this.importPrice = importPrice;
+        this.exportPrice = exportPrice;
+        this.interest = interest;
+        this.bookStatus = bookStatus;
+    }
+
+    public Book() {
+    }
+
+    // methods
+    @Override
+    public void inputData() {
+        System.out.println("<---------------------------------------------------->");
+        boolean isExit = true;
+        // id
+        do {
+            System.out.print("Nhập id sách: ");
+            int id = InputMethods.getInteger();
+            Boolean isExist = false;
+            for (int i = 0; i < books.size(); i++) {
+                if (books.get(i).bookId == id) {
+                    isExist = true;
+                }
+            }
+            if (isExist) {
+                System.err.println("Id đã tồn tại! Vui lòng nhập lại...!!");
+            } else {
+                this.bookId = id;
+                break;
+            }
+        } while (isExit);
+        // name
+        do {
+            System.out.print("Nhập tên sách: ");
+            this.bookName = InputMethods.getString();
+            break;
+        } while (isExit);
+        // title
+        do {
+            System.out.print("Nhập tiêu đề sách: ");
+            String title = InputMethods.getString();
+            if (title.length() <= 10) {
+                System.err.println("Tiêu đề không được dưới 10 kí tự! Vui lòng nhập lại...");
+            } else {
+                this.title = title;
+                break;
+            }
+        } while (isExit);
+        // number of pages
+        do {
+            System.out.print("Nhập số trang sách: ");
+            this.numberOfPages = InputMethods.checkNum();
+            break;
+        } while (isExit);
+        // importPrice
+        do {
+            System.out.print("Nhập đầu vào: ");
+            this.importPrice = InputMethods.checkNum();
+            break;
+        } while (isExit);
+        // exportPrice
+        do {
+            System.out.print("Nhập đầu ra: ");
+            float exportPrice1 = InputMethods.checkNum();
+            if (exportPrice1 < this.importPrice * (0.2)) {
+                System.err.println("Đầu ra không được nhỏ hơn 20% đầu vào!! Vui lòng nhập lại...");
+            } else {
+                this.exportPrice = exportPrice1;
+                break;
+            }
+        } while (isExit);
+        this.interest = this.exportPrice - this.importPrice;
+        // statusBook
+        do {
+            System.out.print("Nhập trạng thái sách(true or false): ");
+            String status = InputMethods.getString();
+            if (status.equals("true") || status.equals("false")) {
+                this.bookStatus = Boolean.parseBoolean(status);
+                break;
+            } else {
+                System.err.println("Yêu cầu nhập đúng trường! Vui lòng nhập lại...");
+            }
+        } while (isExit);
+        System.out.println("<---------------------------------------------------->");
+
+    }
+
+    @Override
+    public void displayData() {
+        System.out.println("<-----------------" + bookName.toUpperCase() + "----------------->");
+        System.out.println("Id sách: " + bookId);
+        System.out.println("Tên sách: " + bookName);
+        System.out.println("Tên tiêu đề: " + title);
+        System.out.println("Số trang sách: " + numberOfPages);
+        System.out.println("Giá nhập sách: " + importPrice);
+        System.out.println("Giá xuất sách: " + exportPrice);
+        System.out.println("Lợi nhuận: " + interest);
+        System.out.println("Trạng thái: " + (bookStatus ? "Đang được bán" : "Lưu trữ"));
+        System.out.println("<------------------------------------------------>");
+
+    }
+}
---
+++ src/ra/run/BookManagement.java
@@ -0,0 +1,205 @@
+package ra.run;
+
+import jdk.internal.util.xml.impl.Input;
+import ra.bussinessImp.Book;
+import ra.config.InputMethods;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class BookManagement {
+    public static List<Book> books = new ArrayList<>();
+    public static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        do {
+            System.out.println("<-----------------MENU----------------->");
+            System.out.println("1. Nhập số sách và nhập thông tin sách\n" +
+                    "2. Hiển thị thông tin các sách\n" +
+                    "3. Sắp xếp sách theo lợi nhuận giảm dần\n" +
+                    "4. Xóa sách theo mã sách\n" +
+                    "5. Tìm kiếm sách theo tên sách\n" +
+                    "6. Thay đổi trạng thái của sách theo mã sách\n" +
+                    "7. Thoát");
+            System.out.print("Nhập chức năng: ");
+            int choose = InputMethods.getByte();
+            switch (choose) {
+                case 1:
+                    add();
+                    break;
+                case 2:
+                    show();
+                    break;
+                case 3:
+                    sortByInteres();
+                    break;
+                case 4:
+                    deleteById();
+                    break;
+                case 5:
+                    searchByName();
+                    break;
+                case 6:
+                    updateStatusById();
+                    break;
+                case 7:
+                    boolean confirm = confirm("Bạn có chắc muốn thoát chương trình(y/n): ");
+                    if (confirm) {
+                        System.out.println("Đã thoát chương trình!!");
+                        return;
+                    } else {
+                        break;
+                    }
+
+                default:
+                    System.out.println("Yêu cầu nhập đúng trường!!");
+                    break;
+            }
+
+
+        } while (true);
+    }
+
+    private static boolean confirm(String note) {
+        do {
+            System.out.print(note);
+            String confirm = InputMethods.getString().toLowerCase();
+            if (confirm.equals("y")) {
+                return true;
+            } else if (confirm.equals("n")) {
+                return false;
+            } else {
+                System.out.println("Yêu cầu nhập đúng trường y/n!!");
+            }
+        } while (true);
+
+    }
+
+    private static int getIndexById(String note) {
+        System.out.print(note);
+        int id = InputMethods.getInteger();
+        int index = -1;
+        for (int i = 0; i < books.size(); i++) {
+            if (books.get(i).getBookId() == id) {
+                index = i;
+            }
+        }
+        if (index == -1) {
+            System.out.println("Id không tồn tại!!");
+        }
+        return index;
+    }
+
+    private static void insertionSort(List<Book> arr) {
+        int n = arr.size();
+        for (int i = 1; i < n; i++) {
+            double key = arr.get(i).getInterest();
+            int j = i - 1;
+            while (j >= 0 && arr.get(j).getInterest() < key) {
+                swap(arr, j, j + 1);
+                j = j - 1;
+            }
+        }
+    }
+
+    private static void swap(List<Book> arr, int i, int j) {
+
+        Book temp = arr.get(i);
+        arr.set(i, arr.get(j));
+        arr.set(j, temp);
+    }
+
+    // main methods
+    private static void updateStatusById() {
+        if (books.size() == 0){
+            System.out.println("Không có sách trong thư viện, yêu cập nhập thêm!!");
+            add();
+        } else {
+            int index = getIndexById("Nhập id muốn cập nhập trạng thái: ");
+            if (index != -1) {
+                books.get(index).setBookStatus(!books.get(index).isBookStatus());
+                System.out.println("Sửa đổi trạng thái thành công!!");
+                books.get(index).displayData();
+            }
+        }
+    }
+
+    private static void sortByInteres() {
+        if (books.size() == 0) {
+            System.out.println("Không có sách trong thư viện, yêu cập nhập thêm!!");
+            add();
+        } else {
+            insertionSort(books);
+            System.out.println("Sau đây là danh sách sau khi đã sắp xếp theo lợi nhuận tăng dần: ");
+            for (Book book : books) {
+                System.out.println(book.getBookName() + " - lợi nhuận: " + book.getInterest());
+            }
+        }
+    }
+
+    private static void searchByName() {
+        if (books.size() == 0) {
+            System.out.println("Không có sách trong thư viện, yêu cập nhập thêm!!");
+            add();
+        } else {
+            System.out.print("Nhập tên sách muốn tìm kiếm: ");
+            String nameBook = InputMethods.getString();
+            int index = -1;
+            for (int i = 0; i < books.size(); i++) {
+                if (books.get(i).getBookName().equals(nameBook)) {
+                    index = i;
+                }
+            }
+            if (index != -1) {
+                books.get(index).displayData();
+            } else {
+                System.out.println("Không có trong danh sách!!");
+            }
+        }
+    }
+
+    private static void deleteById() {
+        if (books.size() == 0) {
+            System.out.println("Không có sách trong thư viện, yêu cập nhập thêm!!");
+            add();
+        } else {
+            do {
+                int index = getIndexById("Nhập id sách muốn xóa: ");
+                if (index != -1) {
+                    boolean confirm = confirm("Bạn có chắc muốn xóa sách " + books.get(index).getBookName() + " không(y/n): ");
+                    if (confirm) {
+                        books.remove(index);
+                        System.out.println("Xóa thành công!!");
+                        break;
+                    } else {
+                        break;
+                    }
+                }
+            } while (true);
+        }
+    }
+
+    private static void show() {
+        if (books.size() == 0) {
+            System.out.println("Không có sách trong thư viện, yêu cập nhập thêm!!");
+            add();
+        } else {
+            for (Book book : books) {
+                book.displayData();
+            }
+        }
+    }
+
+    private static void add() {
+        System.out.print("Nhập số lượng sách muốn nhập: ");
+        int nImport = InputMethods.getInteger();
+        for (int i = 0; i < nImport; i++) {
+            System.out.println("NHẬP THÔNG TIN SÁCH THỨ " + (i + 1));
+            Book book = new Book();
+            book.inputData();
+            books.add(book);
+        }
+        System.out.println("Nhập thành công " + nImport + " sách!");
+    }
+}
